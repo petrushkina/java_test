cc20216c39e27e593ba77c82ebae332ee27e82f1
petrushkina <59146708+petrushkina@users.noreply.github.com>
2021-11-30 23:39:30 +0300

    Вычисление расстояния между двумя точками при помощи метода в классе Point

--- Sandbox/src/main/java/ru/stqa/java/sandbox/MyFirstProject.java
+++ Sandbox/src/main/java/ru/stqa/java/sandbox/MyFirstProject.java
@@ -3,16 +3,14 @@ package ru.stqa.java.sandbox;
 public class MyFirstProject {
 
  public static void main(String[] args) {
-  Point p1 = new Point (3.7, 4.2);
-  Point p2 = new Point (2.1, 7.5);
+  Point p = new Point ();
 
-  System.out.println("Расстояние между двумя точками " + "= " + distance(p1,p2));
+  p.x1 = 2.7;
+  p.y1 = 5.7;
+  p.x2 = 8.3;
+  p.y2 = 6.1;
 
-  System.out.println("Hello, world!");
- }
-
- public static double distance(Point p1, Point p2) {
- return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+  System.out.println("Расстояние между двумя точками " + "= " + p.distance());
  }
 }
 
