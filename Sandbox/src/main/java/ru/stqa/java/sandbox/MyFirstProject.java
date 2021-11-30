d3b8257668798d165eaa7b262c4ab8263e228001
petrushkina <59146708+petrushkina@users.noreply.github.com>
2021-11-30 22:56:42 +0300

    Расчет расстояния между двумя точками

--- Sandbox/src/main/java/ru/stqa/java/sandbox/MyFirstProject.java
+++ Sandbox/src/main/java/ru/stqa/java/sandbox/MyFirstProject.java
@@ -1,9 +1,18 @@
 package ru.stqa.java.sandbox;
 
 public class MyFirstProject {
- 
+
  public static void main(String[] args) {
+  Point p1 = new Point (3.7, 4.2);
+  Point p2 = new Point (2.1, 7.5);
+
+  System.out.println("Расстояние между двумя точками " + "= " + distance(p1,p2));
+
   System.out.println("Hello, world!");
  }
 
-} 
+ public static double distance(Point p1, Point p2) {
+ return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+ }
+}
+
