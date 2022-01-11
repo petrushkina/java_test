7018d50b5c575e8a57210dc0ee086ad90b786006
petrushkina <59146708+petrushkina@users.noreply.github.com>
2022-01-11 19:43:48 +0300

    Автоматическое закрытие файлов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -50,21 +50,20 @@ public class ContactDataGenerator {
     private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
-
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
     }
 
     private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-        Writer writer = new FileWriter(file);
-        for (ContactData contact : contacts) {
-            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s\n",
-                    contact.getLastName(), contact.getFirstName(),
-                    contact.getAddress(), contact.getEmail(), contact.getEmail2(), contact.getEmail3(),
-                    contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone()));
+        try (Writer writer = new FileWriter(file)) {
+            for (ContactData contact : contacts) {
+                writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s\n",
+                        contact.getLastName(), contact.getFirstName(),
+                        contact.getAddress(), contact.getEmail(), contact.getEmail2(), contact.getEmail3(),
+                        contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone()));
+            }
         }
-        writer.close();
     }
 
     private List<ContactData> generateContacts(int count) {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/ContactCreationTests.java
@@ -8,7 +8,6 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,32 +23,34 @@ public class ContactCreationTests extends TestBase {
     @DataProvider
     public Iterator<Object[]> validContact() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.csv"));
-        String line = reader.readLine();
-        while (line != null) {
-            String[] split = line.split(";");
-            list.add(new Object[]{new ContactData()
-                    .withLastName(split[0]).withFirstName(split[1]).withAddress(split[2])
-                    .withEmail(split[3]).withEmail2(split[4]).withEmail3(split[5])
-                    .withHomePhone(split[6]).withMobilePhone(split[7]).withWorkPhone(split[8])});
-            line = reader.readLine();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.csv"))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] split = line.split(";");
+                list.add(new Object[]{new ContactData()
+                        .withLastName(split[0]).withFirstName(split[1]).withAddress(split[2])
+                        .withEmail(split[3]).withEmail2(split[4]).withEmail3(split[5])
+                        .withHomePhone(split[6]).withMobilePhone(split[7]).withWorkPhone(split[8])});
+                line = reader.readLine();
+            }
+            return list.iterator();
         }
-        return list.iterator();
     }
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromJson() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.json"));
-        String json = "";
-        String line = reader.readLine();
-        while (line != null) {
-            json += line;
-            line = reader.readLine();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.json"))) {
+            String json = "";
+            String line = reader.readLine();
+            while (line != null) {
+                json += line;
+                line = reader.readLine();
+            }
+            Gson gson = new Gson();
+            List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+            }.getType());
+            return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
         }
-        Gson gson = new Gson();
-        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
-        }.getType());
-        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
 
@@ -63,15 +64,5 @@ public class ContactCreationTests extends TestBase {
         assertThat(after.size(), equalTo(before.size() + 1));
         assertThat(after, equalTo(
                 before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
-    }
-
-    @Test(enabled = false)
-    public void testCurrentDir() {
-        File currentDir = new File(".");
-        System.out.println(currentDir.getAbsolutePath());
-        File photo = new File("src/test/resources/stru.png");
-        System.out.println(photo.getAbsolutePath());
-        System.out.println(photo.exists());
-
     }
 }
