d5b415028215d432e840b1b57dc76c6c0034bc07
petrushkina <59146708+petrushkina@users.noreply.github.com>
2022-01-17 21:57:01 +0300

    Объединены тесты для проверки информации о контакте в один

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -23,12 +23,14 @@ public class ContactPhoneTests extends TestBase {
     }
 
     @Test
-    public void testContactPhones() {
+    public void testContactInfo() {
         app.goTo().homePage();
         ContactData contact = app.contact().all().iterator().next();
         ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
         assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+        assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+        assertThat(contact.getAddress(), equalTo(reformAddress(contactInfoFromEditForm)));
     }
 
     private String mergePhones(ContactData contact) {
@@ -38,7 +40,19 @@ public class ContactPhoneTests extends TestBase {
                 .collect(Collectors.joining("\n"));
     }
 
+    private String reformAddress(ContactData contact) {
+        return Arrays.asList(contact.getAddress())
+                .stream().filter((s) -> !s.equals(""))
+                .collect(Collectors.joining("\n"));
+    }
+
     public static String cleaned(String phone) {
         return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
+    }
+
+    private String mergeEmails(ContactData contact) {
+        return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
+                .stream().filter((s) -> !s.equals(""))
+                .collect(Collectors.joining("\n"));
     }
 }
